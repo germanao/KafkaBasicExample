0792f5ba45884cfe4af2ed8d87a9e8b680e08f4c
Germano Benini <germano_benini@hotmail.com>
2024-02-06 21:41:59 -0300

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    GroupId = "Group 1",
+    BootstrapServers = "localhost:9092",
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+
+consumer.Subscribe("test-topic");
+
+while (true)
+{
+    try
+    {
+        var consumeResult = consumer.Consume();
+        Console.WriteLine($"Message: {consumeResult.Message.Key} = {consumeResult.Message.Value}");
+    }
+    catch (ConsumeException e)
+    {
+        Console.WriteLine($"Error reading message: {e.Error.Reason}");
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,17 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using var producer = new ProducerBuilder<string, string>(config).Build();
+
+var message = new Message<string, string>
+{
+    Key = Guid.NewGuid().ToString(),
+    Value = "New test message"
+};
+
+var result = await producer.ProduceAsync("test-topic", message);
+Console.WriteLine($"Offset: {result.Offset}");
+Console.WriteLine($"Partition: {result.Partition}");
+Console.WriteLine($"Topic: {result.Topic}");
+Console.WriteLine($"TopicPartition: {result.TopicPartition}");
